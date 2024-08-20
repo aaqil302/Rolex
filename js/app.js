35e791cd4acb9e5b93f9723e437da3720351f201
aaqil302 <aaqilsohail302@gmail.com>
2024-08-20 11:49:28 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('navbar-default');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
+
+
+
